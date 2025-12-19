e90a718233fcc754dfa9674de4a1e50f30c28789
David Silgado Salcedo <dsilgadosalcedo@gmail.com>
2025-12-18 19:59:52 -0500

    fix(imports): update import path for Connecting component in drawing workspace

--- components/workspace/components/drawing-workspace.tsx
+++ components/workspace/components/drawing-workspace.tsx
@@ -3,7 +3,7 @@
 import dynamic from "next/dynamic"
 import { DrawingProvider } from "../../../context/drawing-context"
 import Sidebar from "../../sidebar/components/sidebar"
-import Connecting from "@/components/connecting"
+import Connecting from "../../connecting"
 import { useDrawingInitialization } from "../hooks/use-drawing-initialization"
 
 // Dynamically import the Canvas component and disable SSR
